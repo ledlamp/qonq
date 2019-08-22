e5d27c9811d6e0e6a22c74351812775a5eff3db1
Lamp <ledlamp89@gmail.com>
2019-08-21 17:13:35 -0700

    set uid in ecosystem forgot ofo

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,11 +2,13 @@ module.exports = {
 	"apps": [
 		{
 			"name": "file server v1",
-			"script": "fsv1.js"
+			"script": "fsv1.js",
+			"uid": "www-data"
 		},
 		{
 			"name": "file server v2",
 			"script": "fsv2.js",
+			"uid": "www-data",
 			"log_file": "fsv2.log"
 		}
 	]
