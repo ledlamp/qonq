4f39cb9c52730d9c981ed13ac8595c3aa918cfa5
Lamp <ledlamp89@gmail.com>
2019-09-30 14:52:27 -0700

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,14 +1,12 @@
 module.exports = {
 	"apps": [
 		{
-			"name": "file server v1",
 			"script": "fsv1.js",
-			"uid": "www-data"
+			"uid": "file-server"
 		},
 		{
-			"name": "file server v2",
 			"script": "fsv2.js",
-			"uid": "www-data",
+			"uid": "file-server",
 			"log_file": "fsv2.log"
 		}
 	]
