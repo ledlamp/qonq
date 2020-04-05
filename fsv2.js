f183152ab010614f39627fce49c81c76746d58bc
Lamp <ledlamp89@gmail.com>
2020-04-04 20:16:05 -0700

    finalize major changes
    upload dirs with more than one file will be served like a conventional static web server, with directory listings.
    
    in previous commit i attempted to do my own directory serving but realized it's way easier and better to just rewrite req.url and send it to express static server.

--- fsv2.js
+++ fsv2.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var formidable = require("formidable");
+var serveIndex = require("serve-index");
 var path = require("path");
 var fs = require("fs");
 
@@ -36,47 +37,27 @@ app.post("/upload", (req, res, next) => {
 	});
 });
 
-app.get("*", function(req, res){
-	try {
-		let filecode = req.hostname.split('.')[0]; // for home page just create file in directory named as the last level of base hostname
-		let webroot = path.join(FILES_DIR, filecode);
-		let webrootdirlist = fs.readdirSync(webroot);
-		if (webrootdirlist.length > 1) {
-			// serve like static web server
-			let webpath = path.join(webroot, req.url);
-			if (webpath.length < webroot.length) webpath = webroot; // TODO idk a better way to do this
-			let webfilename = path.basename(req.url);
-			if (fs.statSync(webpath).isDirectory()) {
-				// serve index* file if it exists
-				for (let webfilename of webrootdirlist) {
-					if (webfilename.startsWith("index") && !fs.statSync(webpath).isDirectory())
-						return sendfile(webroot, webfilename, webpath);
-				}
-				// else generate directory listing
-				let webdirlist = fs.readdirSync(webpath);
-				let html = webdirlist.map(fn => `<a href="${fn}">${fn}<a><br>`).join('\n');
-				res.send(html);
-			} else {
-				sendfile(webroot, webfilename, webpath);
-			}
-		} else if (webrootdirlist.length == 1) {
-			// serve the one file
-			sendfile(webroot, webrootdirlist[0]);
-		} else {
-			// bruh
-			res.sendStatus(204);
-		}
-		function sendfile(webroot, filename) {
-			return res.sendFile(filename, {
-				root: path.join(process.cwd(), webroot),
-				headers: {
-					"Content-Disposition": `filename=${filename}`
-				}
-			});
-		}
-	} catch(error) {
-		res.status(error.code == "ENOENT" ? 404 : console.error(error.stack) || 500).send(error.message);
+app.get("*", function(req, res, next){
+	let filecode = req.hostname.split('.')[0]; // for home page just create file in directory named as the last level of base hostname
+	let webroot = path.join(FILES_DIR, filecode);
+	let webrootdirlist = fs.readdirSync(webroot);
+	if (webrootdirlist.length > 1) {
+		req.url = path.join(filecode, req.url);
+		next();
+	} else if (webrootdirlist.length == 1) {
+		res.sendFile(webrootdirlist[0], {
+			root: path.join(process.cwd(), webroot),
+			headers: {"Content-Disposition": `filename=${webrootdirlist[0]}`}
+		});
+	} else {
+		res.sendStatus(204);
 	}
+});
+
+app.get("*", express.static(FILES_DIR), serveIndex(FILES_DIR, {icons: true}));
+
+app.use(function (error, req, res, next) {
+	res.status(error.code == "ENOENT" ? 404 : console.error(error.stack) || 500).send(error.toString());
 });
 
 app.listen(8568, "127.0.0.1");
